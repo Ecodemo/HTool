158d0786232bbe94c0985c49411d3879464decfe
ecodemo <ecodemo@163.com>
2021-04-12 16:36:58 +0800

    清空

--- app/src/main/java/com/ecodemo/magic/box/app/tools/GetPostTool.java
+++
@@ -1,7 +0,0 @@
-package com.ecodemo.magic.box.app.tools;
-import android.app.Activity;
-
-public class GetPostTool extends Activity
-{
-	
-}
--- app/src/main/java/com/ecodemo/magic/box/app/widget/EventInterceptor.java
+++
@@ -1,6 +0,0 @@
-package com.ecodemo.magic.box.app.widget;
-
-public interface EventInterceptor {
-    boolean event();
-}
-
